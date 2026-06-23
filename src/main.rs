38a6246c5a52ef8755c5f76a04641a05c42ecf2c
theammir <the@ammir.dev>
2026-06-23 15:54:15 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+    print!("$ ");
+    io::stdout().flush().unwrap();
 }
