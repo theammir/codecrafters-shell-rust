0d031305a76e6b69e6c755be046a7f9a32067b6a
theammir <the@ammir.dev>
2026-06-23 16:56:35 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 use std::io::{self, Write};
 
 fn main() {
-    let mut command = String::new();
+    let mut input = String::new();
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        io::stdin().read_line(&mut command).unwrap();
-        command = command.trim_end().to_string();
+        io::stdin().read_line(&mut input).unwrap();
+        input = input.trim_end().to_string();
+        let mut arguments = input.split(' ');
+        let executable = arguments.next().unwrap();
+        let arguments: Vec<&str> = arguments.collect();
 
-        if command == "exit" {
+        if executable.is_empty() {
+            continue;
+        } else if executable == "exit" {
             break;
+        } else if executable == "echo" {
+            println!(
+                "{}",
+                arguments
+                    .iter()
+                    .fold(String::new(), |acc, &arg| acc + arg + " ")
+                    .trim_end()
+            )
+        } else {
+            println!("{input}: command not found");
         }
 
-        println!("{command}: command not found");
-        command.clear();
+        input.clear();
     }
 }
